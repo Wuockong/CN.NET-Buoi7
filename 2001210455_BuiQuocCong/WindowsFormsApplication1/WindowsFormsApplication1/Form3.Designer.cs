b09c416bd930ce86a9a1876feb01601966c017a1
Bui Quoc Cong <congbui123123@gmail.com>
2023-10-24 22:46:52 +0700

    Buoi 5

---
+++ 2001210455_BuiQuocCong/WindowsFormsApplication1/WindowsFormsApplication1/Form3.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbSV = new System.Windows.Forms.CheckBox();
+            this.txbTen = new System.Windows.Forms.TextBox();
+            this.txbSL = new System.Windows.Forms.TextBox();
+            this.gbUong = new System.Windows.Forms.GroupBox();
+            this.gbAn = new System.Windows.Forms.GroupBox();
+            this.cbCFDen = new System.Windows.Forms.CheckBox();
+            this.cbCFDa = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.btnTinhTien = new System.Windows.Forms.Button();
+            this.btnNhapLai = new System.Windows.Forms.Button();
+            this.btnThanhToan = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.txbTongTien = new System.Windows.Forms.TextBox();
+            this.txbTongKhach = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gbUong.SuspendLayout();
+            this.gbAn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(282, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CAFE SINH VIÊN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên khách hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số khách hàng";
+            // 
+            // cbSV
+            // 
+            this.cbSV.AutoSize = true;
+            this.cbSV.Location = new System.Drawing.Point(207, 135);
+            this.cbSV.Name = "cbSV";
+            this.cbSV.Size = new System.Drawing.Size(93, 20);
+            this.cbSV.TabIndex = 3;
+            this.cbSV.Text = "Sinh viên ?";
+            this.cbSV.UseVisualStyleBackColor = true;
+            // 
+            // txbTen
+            // 
+            this.txbTen.Location = new System.Drawing.Point(221, 62);
+            this.txbTen.Name = "txbTen";
+            this.txbTen.Size = new System.Drawing.Size(413, 22);
+            this.txbTen.TabIndex = 4;
+            // 
+            // txbSL
+            // 
+            this.txbSL.Location = new System.Drawing.Point(221, 97);
+            this.txbSL.Name = "txbSL";
+            this.txbSL.Size = new System.Drawing.Size(413, 22);
+            this.txbSL.TabIndex = 5;
+            // 
+            // gbUong
+            // 
+            this.gbUong.Controls.Add(this.checkBox6);
+            this.gbUong.Controls.Add(this.checkBox5);
+            this.gbUong.Controls.Add(this.checkBox4);
+            this.gbUong.Controls.Add(this.cbCFDa);
+            this.gbUong.Controls.Add(this.cbCFDen);
+            this.gbUong.Location = new System.Drawing.Point(100, 161);
+            this.gbUong.Name = "gbUong";
+            this.gbUong.Size = new System.Drawing.Size(246, 149);
+            this.gbUong.TabIndex = 6;
+            this.gbUong.TabStop = false;
+            this.gbUong.Text = "Nước uống";
+            // 
+            // gbAn
+            // 
+            this.gbAn.Controls.Add(this.checkBox11);
+            this.gbAn.Controls.Add(this.checkBox10);
+            this.gbAn.Controls.Add(this.checkBox9);
+            this.gbAn.Controls.Add(this.checkBox8);
+            this.gbAn.Controls.Add(this.checkBox7);
+            this.gbAn.Location = new System.Drawing.Point(394, 161);
+            this.gbAn.Name = "gbAn";
+            this.gbAn.Size = new System.Drawing.Size(240, 149);
+            this.gbAn.TabIndex = 7;
+            this.gbAn.TabStop = false;
+            this.gbAn.Text = "Thức ăn";
+            // 
+            // cbCFDen
+            // 
+            this.cbCFDen.AutoSize = true;
+            this.cbCFDen.Location = new System.Drawing.Point(8, 31);
+            this.cbCFDen.Name = "cbCFDen";
+            this.cbCFDen.Size = new System.Drawing.Size(83, 20);
+            this.cbCFDen.TabIndex = 0;
+            this.cbCFDen.Text = "Cafe đen";
+            this.cbCFDen.UseVisualStyleBackColor = true;
+            // 
+            // cbCFDa
+            // 
+            this.cbCFDa.AutoSize = true;
+            this.cbCFDa.Location = new System.Drawing.Point(128, 31);
+            this.cbCFDa.Name = "cbCFDa";
+            this.cbCFDa.Size = new System.Drawing.Size(76, 20);
+            this.cbCFDa.TabIndex = 1;
+            this.cbCFDa.Text = "Cafe đá";
+            this.cbCFDa.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 68);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(82, 20);
+            this.checkBox4.TabIndex = 2;
+            this.checkBox4.Text = "Cafe sữa";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(128, 68);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(101, 20);
+            this.checkBox5.TabIndex = 3;
+            this.checkBox5.Text = "Cafe sữa đá";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(8, 105);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(86, 20);
+            this.checkBox6.TabIndex = 4;
+            this.checkBox6.Text = "Cafe kem";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(9, 31);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(109, 20);
+            this.checkBox7.TabIndex = 0;
+            this.checkBox7.Text = "Bánh mì trứng";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(124, 31);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(91, 20);
+            this.checkBox8.TabIndex = 1;
+            this.checkBox8.Text = "Mỳ xào bò";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(9, 68);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(124, 20);
+            this.checkBox9.TabIndex = 2;
+            this.checkBox9.Text = "Bánh mỳ chả cá";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(9, 105);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(72, 20);
+            this.checkBox10.TabIndex = 3;
+            this.checkBox10.Text = "Mỳ cay";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(124, 105);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(104, 20);
+            this.checkBox11.TabIndex = 4;
+            this.checkBox11.Text = "Mỳ tôm trứng";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // btnTinhTien
+            // 
+            this.btnTinhTien.Location = new System.Drawing.Point(152, 327);
+            this.btnTinhTien.Name = "btnTinhTien";
+            this.btnTinhTien.Size = new System.Drawing.Size(81, 23);
+            this.btnTinhTien.TabIndex = 8;
+            this.btnTinhTien.Text = "Tính tiền";
+            this.btnTinhTien.UseVisualStyleBackColor = true;
+            this.btnTinhTien.Click += new System.EventHandler(this.btnTinhTien_Click);
+            // 
+            // btnNhapLai
+            // 
+            this.btnNhapLai.Location = new System.Drawing.Point(271, 327);
+            this.btnNhapLai.Name = "btnNhapLai";
+            this.btnNhapLai.Size = new System.Drawing.Size(75, 23);
+            this.btnNhapLai.TabIndex = 9;
+            this.btnNhapLai.Text = "Nhập lại";
+            this.btnNhapLai.UseVisualStyleBackColor = true;
+            // 
+            // btnThanhToan
+            // 
+            this.btnThanhToan.Enabled = false;
+            this.btnThanhToan.Location = new System.Drawing.Point(394, 327);
+            this.btnThanhToan.Name = "btnThanhToan";
+            this.btnThanhToan.Size = new System.Drawing.Size(104, 23);
+            this.btnThanhToan.TabIndex = 10;
+            this.btnThanhToan.Text = "Thanh toán";
+            this.btnThanhToan.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(547, 327);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 11;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // txbTongTien
+            // 
+            this.txbTongTien.Location = new System.Drawing.Point(238, 403);
+            this.txbTongTien.Name = "txbTongTien";
+            this.txbTongTien.Size = new System.Drawing.Size(396, 22);
+            this.txbTongTien.TabIndex = 15;
+            // 
+            // txbTongKhach
+            // 
+            this.txbTongKhach.Location = new System.Drawing.Point(238, 368);
+            this.txbTongKhach.Name = "txbTongKhach";
+            this.txbTongKhach.Size = new System.Drawing.Size(396, 22);
+            this.txbTongKhach.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Tổng tiền thanh toán";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 371);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Tổng khách hàng";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbTongTien);
+            this.Controls.Add(this.txbTongKhach);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThanhToan);
+            this.Controls.Add(this.btnNhapLai);
+            this.Controls.Add(this.btnTinhTien);
+            this.Controls.Add(this.gbAn);
+            this.Controls.Add(this.gbUong);
+            this.Controls.Add(this.txbSL);
+            this.Controls.Add(this.txbTen);
+            this.Controls.Add(this.cbSV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.gbUong.ResumeLayout(false);
+            this.gbUong.PerformLayout();
+            this.gbAn.ResumeLayout(false);
+            this.gbAn.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox cbSV;
+        private System.Windows.Forms.TextBox txbTen;
+        private System.Windows.Forms.TextBox txbSL;
+        private System.Windows.Forms.GroupBox gbUong;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox cbCFDa;
+        private System.Windows.Forms.CheckBox cbCFDen;
+        private System.Windows.Forms.GroupBox gbAn;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.Button btnTinhTien;
+        private System.Windows.Forms.Button btnNhapLai;
+        private System.Windows.Forms.Button btnThanhToan;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.TextBox txbTongTien;
+        private System.Windows.Forms.TextBox txbTongKhach;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ 2001210455_BuiQuocCong/WindowsFormsApplication1/WindowsFormsApplication1/Form3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txbTen.Text) && !string.IsNullOrEmpty(txbSL.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc là muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
